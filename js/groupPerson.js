e90e1e3698bc33cacc7643937f5cba3a0cd5dc26
FrankYeah <aaa24295234@gmail.com>
2020-11-20 00:59:07 +0800

    person

---
+++ js/groupPerson.js
@@ -0,0 +1,102 @@
+var img1 = document.getElementById("img1");
+var popup1 = document.getElementById("popup1");
+var close1 = document.getElementById("close1");
+
+img1.addEventListener('click', function(){
+	popup1.style.display = 'flex';
+})
+
+close1.addEventListener('click', function(){
+	popup1.style.display = 'none';
+})
+
+
+var img2 = document.getElementById("img2");
+var popup2 = document.getElementById("popup2");
+var close2 = document.getElementById("close2");
+
+img2.addEventListener('click', function(){
+	popup2.style.display = 'flex';
+})
+
+close2.addEventListener('click', function(){
+	popup2.style.display = 'none';
+})
+
+
+var img3 = document.getElementById("img3");
+var popup3 = document.getElementById("popup3");
+var close3 = document.getElementById("close3");
+
+img3.addEventListener('click', function(){
+	popup3.style.display = 'flex';
+})
+
+close3.addEventListener('click', function(){
+	popup3.style.display = 'none';
+})
+
+
+var img4 = document.getElementById("img4");
+var popup4 = document.getElementById("popup4");
+var close4 = document.getElementById("close4");
+
+img4.addEventListener('click', function(){
+	popup4.style.display = 'flex';
+})
+
+close4.addEventListener('click', function(){
+	popup4.style.display = 'none';
+})
+
+
+var img5 = document.getElementById("img5");
+var popup5 = document.getElementById("popup5");
+var close5 = document.getElementById("close5");
+
+img5.addEventListener('click', function(){
+	popup5.style.display = 'flex';
+})
+
+close5.addEventListener('click', function(){
+	popup5.style.display = 'none';
+})
+
+
+var img6 = document.getElementById("img6");
+var popup6 = document.getElementById("popup6");
+var close6 = document.getElementById("close6");
+
+img6.addEventListener('click', function(){
+	popup6.style.display = 'flex';
+})
+
+close6.addEventListener('click', function(){
+	popup6.style.display = 'none';
+})
+
+
+var img7 = document.getElementById("img7");
+var popup7 = document.getElementById("popup7");
+var close7 = document.getElementById("close7");
+
+img7.addEventListener('click', function(){
+	popup7.style.display = 'flex';
+})
+
+close7.addEventListener('click', function(){
+	popup7.style.display = 'none';
+})
+
+
+var img8 = document.getElementById("img8");
+var popup8 = document.getElementById("popup8");
+var close8 = document.getElementById("close8");
+
+img8.addEventListener('click', function(){
+	popup8.style.display = 'flex';
+})
+
+close8.addEventListener('click', function(){
+	popup8.style.display = 'none';
+})

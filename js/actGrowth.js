2ea48e2289b99632ebef25a74dcbda35cddde6c8
FrankYeah <aaa24295234@gmail.com>
2020-11-26 02:26:49 +0800

    swiper

---
+++ js/actGrowth.js
@@ -0,0 +1,35 @@
+var open1 = document.getElementById("open1");
+var popup1 = document.getElementById("popup1");
+var close1 = document.getElementById("close1");
+
+open1.addEventListener('click', function(){
+    popup1.style.display = 'flex';
+})
+
+close1.addEventListener('click', function(){
+    popup1.style.display = 'none';
+})
+
+var open2 = document.getElementById("open2");
+var popup2 = document.getElementById("popup2");
+var close2 = document.getElementById("close2");
+
+open2.addEventListener('click', function(){
+    popup2.style.display = 'flex';
+})
+
+close2.addEventListener('click', function(){
+    popup2.style.display = 'none';
+})
+
+var open3 = document.getElementById("open3");
+var popup3 = document.getElementById("popup3");
+var close3 = document.getElementById("close3");
+
+open3.addEventListener('click', function(){
+    popup3.style.display = 'flex';
+})
+
+close3.addEventListener('click', function(){
+    popup3.style.display = 'none';
+})
